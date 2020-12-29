6721d7cd9ba2eb1b55adc1cf0a680deaca0a3491
sanket2498 <sanketdulange24@gmail.com>
2020-12-29 09:58:25 +0530

    Added UC2

--- src/main/java/com/demogit/Main.java
+++ src/main/java/com/demogit/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String args[])
     {
         System.out.println("Sanket");
+        System.out.println("Dulange");
+        System.out.println("P");
     }
 }
